35ec78d512134d45683e388079daf6966e4965cb
Алексей Коротков <krtkv.alex@mail.ru>
2022-03-27 14:50:52 +0500

    В этом комите добавлены urls и venw-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Главная страница проекта Yatube')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Все посты группы {slug}')
