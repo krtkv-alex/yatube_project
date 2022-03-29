b69d302a02d016cd1eccae369055140e1d321b8f
Алексей Коротков <krtkv.alex@mail.ru>
2022-03-29 22:13:54 +0500

    Комит перед сдачей проекта 3 спринта.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,23 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Group, Post
 
 
 def index(request):
     template = 'posts/index.html'
-    text = 'Это главная страница проекта Yatube'
+    text = 'Главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'text': text
+        'text': text,
+        'posts': posts
     }
     return render(request, template, context)
 
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Yatube'
     context = {
-        'text': text
+        'group': group,
+        'posts': posts
     }
     return render(request, template, context)
