7d8396b4199f40440c516642899b9e77efd65bc0
Алексей Коротков <krtkv.alex@mail.ru>
2022-03-29 18:40:06 +0500

    Настроены шаблоны, лого и css

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse('Главная страница проекта Yatube')
+    template = 'posts/index.html'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f'Все посты группы {slug}')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
